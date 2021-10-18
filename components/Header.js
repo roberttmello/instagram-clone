2cae745f006fb9d7d6ac1c6dc76cffddfa24af68
roberttmello <roberttmello@protonmail.com>
2021-10-18 12:58:32 -0300

    Header completed.

--- components/Header.js
+++ components/Header.js
@@ -9,10 +9,12 @@ import {
   MenuIcon,
 } from "@heroicons/react/outline";
 
+import { HomeIcon } from "@heroicons/react/solid";
+
 const Header = () => {
   return (
     <div>
-      <div className="flex justify-between bg-white max-w-6xl">
+      <div className="flex justify-between bg-white max-w-6xl mx-5 lg:mx-auto">
         {/* Left block */}
         <div className="relative hidden lg:inline-grid w-24  cursor-pointer">
           <Image src="/images/logo.png" layout="fill" objectFit="contain" />
@@ -29,7 +31,6 @@ const Header = () => {
         {/* Middle block */}
         <div className="max-w-xs">
           <div className="relative mt-1 p-3 rounded-md">
-
             <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
               <SearchIcon className="h-5 w-5 text-gray-500" />
             </div>
@@ -43,9 +44,23 @@ const Header = () => {
         </div>
 
         {/* Right block */}
-        <div>
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <MenuIcon className="h-6 md:hidden cursor-pointer" />
 
+          <div className="relative navBtn">
+            <PaperAirplaneIcon className="navBtn rotate-45" />
+            <div className="absolute -top-1 -right-1 text-xs
+            w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
+          </div>
+       
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
+          <img src="https://media-exp1.licdn.com/dms/image/C4D03AQHSw8Zbs02gIA/profile-displayphoto-shrink_800_800/0/1623539859388?e=1640217600&v=beta&t=wVHfnP8I3CKAKRPTs7pW5z-L4z3SyGAIrfmP7uS3IJM" alt="Profile image"
+          className="h-10 rounded-full cursor-pointer"/>
         </div>
+
       </div>
     </div>
   );
